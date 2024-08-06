44fd60b7f917b9f4cb8410613f935763547b2acc
花木凋零成兰 <18570354653@163.com>
2024-08-06 11:48:56 +0800

    ICP备案

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -33,10 +33,10 @@ const blogTheme = getThemeConfig({
     // message 字段支持配置为HTML内容，配置多条可以配置为数组
     // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
     copyright: 'MIT License | 花木凋零成兰',
-    // icpRecord: {
-    //   name: '蜀ICP备19011724号',
-    //   link: 'https://beian.miit.gov.cn/'
-    // },
+    icpRecord: {
+      name: '湘ICP备2024077074号',
+      link: 'https://beian.miit.gov.cn/'
+    },
     // securityRecord: {
     //   name: '公网安备',
     //   link: 'https://www.beian.gov.cn/portal/index.do'
